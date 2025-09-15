656eca8a09bf7f6496ca9bbafb8cae7968e99ded
jyuuuuu0 <s25023@gsm.hs.kr>
2025-09-15 17:51:07 +0900

    feat: signin page

--- src/views/signin/ui/index.jsx
+++ src/views/signin/ui/index.jsx
@@ -1,19 +1,21 @@
 import { Link } from 'react-router-dom';
+import "@fontsource/bungee"; 
 
 function SigninViews(){
 
     return(
-        <div className='w-full h-screen bg-[#F8FAFC] flex justify-center items-center'>
-            <div className='bg-[#D9EAFD] flex flex-col justify-center items-center rounded-[16px] w-[440px] h-[390px]'>
-                <form className='flex flex-col gap-[21px] pt-[20px]'> 
-                    <input type="text" placeholder='이메일' className='bg-white border-0 text-[16px] font-normal rounded-[5px] pl-[16px] w-[383px] h-[45px] text-[#1B5087] placeholder:text-[#D9D9D9]'></input>
-                    <input type="password" placeholder='비밀번호' className='bg-white border-0 text-[16px] font-normal rounded-[5px] pl-[16px] w-[383px] h-[45px] text-[#1B5087] placeholder:text-[#D9D9D9]'></input>
-                    <button type='submit' className='bg-[#95C4F5] text-white border-0 rounded-[5px] w-[383px] h-[45px] text-[16px] font-semibold'>로그인</button>
+        <div className="w-screen h-screen bg-[#FCFCFC] flex justify-center items-center">
+            <div className="bg-white flex flex-col justify-center items-center rounded-2xl w-[30rem] h-[25rem] mt-9">
+                <p className="flex justify-center items-center font-['Bungee'] text-[#95C4F5] text-4xl m-0">NODE</p>
+                <form className="flex flex-col items-center pt-10 gap-5 w-full"> 
+                    <input type="text" placeholder='이메일' className="border border-[#D9D9D9] text-base font-normal rounded-xl w-5/6 h-12 px-3 text-[#1B5087] placeholder-[#D9D9D9]"></input>
+                    <input type="password" placeholder='비밀번호' className="border border-[#D9D9D9] text-base font-normal rounded-xl w-5/6 h-12 px-3 text-[#1B5087] placeholder-[#D9D9D9]"></input>
+                    <button type='submit' className="bg-[#95C4F5] text-white rounded-xl w-5/6 h-12 text-base font-semibold">로그인</button>
                 </form>
 
-                <div className='flex text-[12px] mt-[21px] gap-[5px]'>
-                    <span className='text-[#979797]'>NODE가 처음이신가요?</span>
-                    <Link to="/signup" className='text-[#1B5087]'>회원가입</Link>
+                <div className="flex text-xs mt-5 gap-1">
+                    <span className="text-[#979797]">NODE가 처음이신가요?</span>
+                    <Link to="/signup" className="text-[#1B5087]">회원가입</Link>
                 </div>
             </div>
         </div>
