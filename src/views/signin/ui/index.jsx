fe1d21ef4c734d41f19f014fe59573ed4b606c47
jyuuuuu0 <s25023@gsm.hs.kr>
2025-09-15 18:03:35 +0900

    feat: signin page

--- src/views/signin/ui/index.jsx
+++ src/views/signin/ui/index.jsx
@@ -6,7 +6,7 @@ function SigninViews(){
     return(
         <div className="w-screen h-screen bg-[#FCFCFC] flex justify-center items-center">
             <div className="bg-white flex flex-col justify-center items-center rounded-2xl w-[30rem] h-[25rem] mt-9">
-                <p className="flex justify-center items-center font-['Bungee'] text-[#95C4F5] text-4xl m-0">NODE</p>
+                <p className="flex justify-center items-center font-['Bungee'] text-[#95C4F5] text-[3rem] m-0">NODE</p>
                 <form className="flex flex-col items-center pt-10 gap-5 w-full"> 
                     <input type="text" placeholder='이메일' className="border border-[#D9D9D9] text-base font-normal rounded-xl w-5/6 h-12 px-3 text-[#1B5087] placeholder-[#D9D9D9]"></input>
                     <input type="password" placeholder='비밀번호' className="border border-[#D9D9D9] text-base font-normal rounded-xl w-5/6 h-12 px-3 text-[#1B5087] placeholder-[#D9D9D9]"></input>
