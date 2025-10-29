4f84ab67e99de66f2371a783a5a3bd5d2bac3c32
jyuuuuu0 <s25023@gsm.hs.kr>
2025-10-29 23:46:14 +0900

    feat: Bungee 폰트 Tailwind 설정 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,13 @@ module.exports = {
     content: [
         "./src/**/*.{js,jsx,ts,tsx}",
     ],
-    theme: { extend: {} },
+    theme: {
+        extend: {
+            fontFamily: {
+                tmoney: ["TmoneyRoundWind", "sans-serif"],
+                bungee: ["Bungee", "sans-serif"],
+            },
+        },
+    },
     plugins: [],
-}
+};
