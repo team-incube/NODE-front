ebf8d49c50f77ee6333138ca5dab93e3a1620ad8
jyuuuuu0 <s25023@gsm.hs.kr>
2025-11-03 19:43:50 +0900

    feat: 회원가입 step1 로고, 설명 추가

---
+++ src/pages/signup/step1.jsx
@@ -0,0 +1,20 @@
+
+function Step1() {
+
+    return (
+        <div className="flex items-center justify-center bg-[#FCFCFC] w-screen h-screen">
+            <div className="flex flex-col items-center bg-white w-[46.875rem] h-[30.75rem] rounded-[1.5rem] gap-[1.25rem]">
+                <p className="font-bungee text-[#95C4F5] text-[3rem] mt-[2.25rem]">NODE</p>
+                <p className="font-tmoney font-normal text-[#95C4F5] text-[1rem]">역할을 선택해주세요!</p>
+                <div>
+
+                </div>
+                <div>
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Step1;
