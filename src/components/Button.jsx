ee4259c9536f94048af7efbec5c2af89bf31d3d3
jyuuuuu0 <s25023@gsm.hs.kr>
2025-11-03 21:04:51 +0900

    feat: 멘티, 멘토 버튼 클릭시 버튼과 svg 색상 변경

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,7 @@
-function Button({ className, children, selected }) {
+function Button({ className, children, selected, onClick }) {
     return (
         <button
-
+            onClick={onClick}
             className={`${className} font-tmoney font-extrabold text-white rounded-2xl ${selected ? "bg-[#95C4F5]" : "bg-[#D9D9D9]"
                 }`}>
             {children}
--- src/pages/signup/step1.jsx
+++ src/pages/signup/step1.jsx
@@ -1,9 +1,13 @@
+import { useState } from "react";
 import Lamp from "../../assets/icons/lamp";
 import Sprout from "../../assets/icons/sprout";
 import Button from "../../components/Button";
 
 function Step1() {
-
+    const [selected, setSelected] = useState(null);
+    const handleSelect = (role) => {
+        setSelected(selected === role ? null : role);
+    }
     return (
         <div className="flex items-center justify-center bg-[#FCFCFC] w-screen h-screen">
             <div className="flex flex-col items-center bg-white w-[46.875rem] rounded-[1.5rem] gap-[1.25rem] pb-[36px]">
@@ -11,12 +15,24 @@ function Step1() {
                 <p className="font-tmoney font-normal text-[#95C4F5] text-[1rem]">역할을 선택해주세요!</p>
                 <div className="flex flex-row gap-[2.25rem]">
                     <div className="flex items-center justify-end flex-col gap-[4.625rem] pb-[1.1875rem] border w-[19.9375rem] h-[18.75rem] rounded-2xl">
-                        <Sprout />
-                        <Button className="w-[17.1875rem] h-[3rem]">멘티</Button>
+                        <Sprout color={selected === "mentee" ? "#95C4F5" : "#D9D9D9"} />
+                        <Button
+                            className="w-[17.1875rem] h-[3rem]"
+                            selected={selected === "mentee"}
+                            onClick={() => handleSelect("mentee")}
+                        >
+                            멘티
+                        </Button>
                     </div>
                     <div className="flex items-center justify-end flex-col gap-[4.625rem] pb-[1.1875rem] border w-[19.9375rem] h-[18.75rem] rounded-2xl">
-                        <Lamp />
-                        <Button className="w-[17.1875rem] h-[3rem]">멘토</Button>
+                        <Lamp color={selected === "mentor" ? "#95C4F5" : "#D9D9D9"} />
+                        <Button
+                            className="w-[17.1875rem] h-[3rem]"
+                            selected={selected === "mentor"}
+                            onClick={() => handleSelect("mentor")}
+                        >
+                            멘토
+                        </Button>
                     </div>
                 </div>
             </div>
