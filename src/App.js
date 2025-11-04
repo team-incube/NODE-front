8381ed4cf4144b5203248dcdade6f8cc39b2d984
jyuuuuu0 <s25023@gsm.hs.kr>
2025-11-04 16:11:54 +0900

    feat: 회원가입 step1~4 라우트 설정

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import { Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Step1 from "./pages/signup/step1";
+import Step2 from "./pages/signup/step2";
+import Step3 from "./pages/signup/step3";
+import Step4 from "./pages/signup/step4";
 
 function App() {
     return (
         <Routes>
             <Route path="step1" element={<Step1 />} />
+            <Route path="step2" element={<Step2 />} />
+            <Route path="step3" element={<Step3 />} />
+            <Route path="step4" element={<Step4 />} />
         </Routes>
     )
 }
