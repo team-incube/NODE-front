28dad8cee617df5fccff81744f82417e01793489
ma94275 <s25020@gsm.hs.kr>
2025-11-06 20:55:00 +0900

    feat: 취소, 제출 버튼 컴포넌트화

---
+++ src/components/Btn.jsx
@@ -0,0 +1,13 @@
+export default function Btn({value, variant}){
+    const base = "w-[140px] h-[46px] rounded-[12px] font-tmoney font-normal"
+    const variants = {
+        cancel: "text-white bg-[#95C4F5]",
+        submit: "text-[#95C4F5] bg-white border-[1px] border-[#95C4F5]",
+    };
+
+    return(
+        <button className={`${base} ${variants[variant]}`}>
+            {value}
+        </button>
+    );
+}
