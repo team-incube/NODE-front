56b1163b628e434ae22092e9608ee9b63ab0e03a
ma94275 <s25020@gsm.hs.kr>
2025-11-06 21:05:12 +0900

    repactor: border 수정

--- src/components/Btn.jsx
+++ src/components/Btn.jsx
@@ -2,7 +2,7 @@ export default function Btn({value, variant}){
     const base = "w-[140px] h-[46px] rounded-[12px] font-tmoney font-normal"
     const variants = {
         cancel: "text-white bg-[#95C4F5]",
-        submit: "text-[#95C4F5] bg-white border-[1px] border-[#95C4F5]",
+        submit: "text-[#95C4F5] bg-white border border-[#95C4F5]",
     };
 
     return(
