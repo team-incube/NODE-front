f8bc864edd4fe21e10dd989c383a92978c2e3dcf
jyuuuuu0 <s25023@gsm.hs.kr>
2025-11-27 19:43:45 +0900

    fix: step2 불러오기 삭제

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { Routes, Route } from "react-router-dom";
 import Step1 from "./pages/signup/step1";
-import Step2 from "./pages/signup/step2";
 import Step3 from "./pages/signup/step3";
 import Step4 from "./pages/signup/step4";
 
@@ -8,7 +7,6 @@ function App() {
     return (
         <Routes>
             <Route path="step1" element={<Step1 />} />
-            <Route path="step2" element={<Step2 />} />
             <Route path="step3" element={<Step3 />} />
             <Route path="step4" element={<Step4 />} />
         </Routes>
