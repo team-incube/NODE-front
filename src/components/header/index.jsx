2cc19c8e48404d0e7866845adbfeb56dc9cd8c21
jyuuuuu0 <s25023@gsm.hs.kr>
2025-11-27 23:52:35 +0900

    feat: 링크 데이터 분리 후 Link 태그로 변경

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,14 +1,23 @@
+import { Link } from "react-router-dom";
 import PopUp from "../../assets/icons/popUp";
+import { HEADER_NAV } from "../../config/header/navigation";
 
 export default function Header() {
     return (
         <header className="w-full min-h-[70px] flex flex-col">
             <div className="flex flex-row justify-between items-center px-20 py-[13.5px]">
-                <p className="font-bungee text-[#95C4F5] text-[1.75rem]">NODE</p>
+                <Link href="/" className="font-bungee text-[#95C4F5] text-[1.75rem]">NODE</Link>
                 <div className="flex flex-row items-center gap-[3.75rem]">
                     <PopUp />
-                    <p className="text-[1.25rem]">과제리스트</p>
-                    <p className="text-[1.25rem]">My</p>
+                    {HEADER_NAV.map((item) => (
+                        <Link 
+                            key={item.path} 
+                            to={item.path} 
+                            className="text-[1.25rem]"
+                        >
+                            {item.label}
+                        </Link>
+                    ))}
                 </div>
             </div>
             <hr className="border-1 border-[#D9D9D9]" />
