666d2465c9e3bda66644c9cee659564f5c0b4c4f
jyuuuuu0 <s25023@gsm.hs.kr>
2026-01-05 16:04:20 +0900

    fix: 경로 수정

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import { Routes, Route } from "react-router-dom";
+import Step2 from "./pages/signup/step2";
 import Step1 from "./pages/signup/step1";
 import Step3 from "./pages/signup/step3";
 import Step4 from "./pages/signup/step4";
 
 function App() {
-    return (
-        <Routes>
-            <Route path="step1" element={<Step1 />} />
-            <Route path="step3" element={<Step3 />} />
-            <Route path="step4" element={<Step4 />} />
-        </Routes>
-    )
+  return (
+    <Routes>
+      <Route path="/signup-step1" element={<Step1 />} />
+      <Route path="/signup-step2" element={<Step2 />} />
+      <Route path="/signup-step3" element={<Step3 />} />
+      <Route path="/signup-step4" element={<Step4 />} />
+    </Routes>
+  );
 }
 
 export default App;
