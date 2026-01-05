76d4dc98cfa7918179d01e89ca6aff4c2619d17d
jyuuuuu0 <s25023@gsm.hs.kr>
2026-01-05 16:04:09 +0900

    feat: 버튼 생성

---
+++ src/components/signup/Button.jsx
@@ -0,0 +1,12 @@
+export default function Button({ className, children, selected, onClick }) {
+  return (
+    <button
+      onClick={onClick}
+      className={`${className} font-tmoney font-extrabold text-white rounded-2xl ${
+        selected ? "bg-[#95C4F5]" : "bg-[#D9D9D9]"
+      }`}
+    >
+      {children}
+    </button>
+  );
+}
