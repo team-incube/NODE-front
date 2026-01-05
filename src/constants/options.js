350fec55b6ff40d1638025bbe79e56a94a622d41
jyuuuuu0 <s25023@gsm.hs.kr>
2026-01-05 16:03:51 +0900

    feat: 옵션 추가

--- src/constants/options.js
+++ src/constants/options.js
@@ -1,2 +1,6 @@
-export const GRADES = ['1학년', '2학년', '3학년'];
-export const CLASSES = ['1반', '2반', '3반', '4반'];
+export const GRADES = ["1학년", "2학년", "3학년"];
+export const CLASSES = ["1반", "2반", "3반", "4반"];
+export const GENDERS = [
+  { value: "male", label: "남자" },
+  { value: "female", label: "여자" },
+];
